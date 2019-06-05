7cfed383a2e861a1f16bccd9ea95db05fb3a0a0b
vishaluchil <vishaluchil99@outlook.com>
2019-06-06 01:55:22 +0530

    host to connection string

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-cubic-88279',
-        user: 'postgres',
-        password: 'v',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 

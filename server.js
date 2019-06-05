876914f0f7a8f25a83175648ee7787873b9a6568
vishaluchil <vishaluchil99@outlook.com>
2019-06-05 22:45:46 +0530

    port changed

--- server.js
+++ server.js
@@ -37,4 +37,4 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.POR|| 3000, () => console.log(`app is running on port ${process.env.PORT}`))
+app.listen(process.env.PORT || 3000, () => console.log(`app is running on port ${process.env.PORT}`))

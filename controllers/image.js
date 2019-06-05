3be5475d8aaca7df50eed6d93cc118cfd435a97e
vishaluchil <vishaluchil9@outlook.com>
2019-06-05 20:38:26 +0530

    adding smart-brain-api

---
+++ controllers/image.js
@@ -0,0 +1,27 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'f51abe9ad97642c797e926aec6749bb2'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work with api'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {res.json(entries[0])})
+    .catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+};
---
+++ controllers/profile.js
@@ -0,0 +1,18 @@
+const handleProfileGet = (req, res, db) => {
+    const {
+        id
+    } = req.params;
+    db.select('*').from('users').where({id})
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('Not Found');
+            }
+        })
+        .catch(res.status(400).json('Error getting user'))
+}
+
+module.exports = {
+    handleProfileGet: handleProfileGet
+};
